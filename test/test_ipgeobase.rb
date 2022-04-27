6f0736974307d54446227e685c0d63ea8e3c1bb5
Sergey <ser_8904@mail.ru>
2022-04-27 17:59:08 +0300

    add tests

--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -7,7 +7,16 @@ class TestIpgeobase < Minitest::Test
     refute_nil ::Ipgeobase::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_ipgeobase_lookup
+    stub_http_request(:get, 'http://ip-api.com/xml/8.8.8.8')
+      .to_return(status: 200, body: File.read('test/fixtures/query.xml'))
+
+    ip_meta = Ipgeobase.lookup('8.8.8.8')
+
+    assert_equal('Montreal', ip_meta.city)
+    assert_equal('Canada', ip_meta.country)
+    assert_equal('CA', ip_meta.country_code)
+    assert_equal(45.4995, ip_meta.lat)
+    assert_equal(-73.5848, ip_meta.lon)
   end
 end
